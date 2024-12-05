91a3be636e476406906c117ea700a5883f1ea524
soumya9346 <maddirlasoumya100@gmail.com>
2024-12-05 13:20:49 +0530

    interview purpose javascript

---
+++ MathObjects/date.js
@@ -0,0 +1,11 @@
+var d = new Date(2000,8);
+console.log(d);
+
+var dt = d.getDate();
+console.log(dt);
+
+var dtt = d.getFullYear();
+console.log(dtt);
+
+var df = d.getMilliseconds();
+console.log(df);
---
+++ MathObjects/math.js
@@ -0,0 +1,18 @@
+var a = 27.867;
+// number to integer
+
+console.log(Math.round(a))
+console.log(Math.ceil(a));
+console.log(Math.floor(a));
+console.log(Math.trunc(a));
+
+// Math properties
+
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+console.log(Math.SQRT1_2);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
